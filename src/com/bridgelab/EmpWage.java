5e0f9bb6f77f6c62accb4ad7527977d428635ee0
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-11-10 20:32:53 +0530

    UC9 - Total Wage for Each Company

--- src/com/bridgelab/EmpWage.java
+++ src/com/bridgelab/EmpWage.java
@@ -1,48 +1,60 @@
 package com.bridgelab;
-// UC-8 Compute Employee Wage for multiple companies
+// UC-9 Ability to save the Total Wage for Each Company
 import static java.lang.Math.random;
 public class EmpWage {
-    static final int IS_FULL_TIME = 1;
-    static final int IS_PART_TIME = 2;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
 //    static final int EMP_WAGE_PER_HOUR = 20;
 //    static final int NUM_WORKING_DAYS = 20;
 //    static  final int MAX_WORKING_HOURS = 100;
+    private final String company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
+    private int totalEmpWage;
 
-    public static void EMPWage(String company, int EMP_WAGE_PER_HOUR, int NUM_WORKING_DAYS, int MAX_WORKING_HOURS) {
-        int empHrs = 0;
-        int wage = 0;
-        int totalSalaryPerMonth = 0;
-        int totalWorkingDays = 0;
+    public EmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+    }
+    public void computeEmpWage() {
 
-        System.out.println("Company name :::: "+company);
-        while (totalWorkingDays <= NUM_WORKING_DAYS && totalWorkingDays <= MAX_WORKING_HOURS){
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays <= numOfWorkingDays){
             int empCheck = (int) (random() * 3);
             totalWorkingDays++;
-            switch (empCheck){
-                case IS_FULL_TIME :
+            switch (empCheck) {
+                case IS_FULL_TIME -> {
                     System.out.println("Employee is Present.");
                     empHrs = 8;
-                    break;
-                case IS_PART_TIME:
+                }
+                case IS_PART_TIME -> {
                     System.out.println("Employee is Part time present");
                     empHrs = 4;
-                    break;
-                default:
+                }
+                default -> {
                     System.out.println("Employee is Absent");
                     empHrs = 0;
+                }
             }
-            wage += empHrs;
-            System.out.println("Total Working Hours :: "+wage+" \nTotal Working Days ::"+totalWorkingDays);
+            totalEmpHrs += empHrs;
+            System.out.println("Total Working Hours :: "+empHrs+" \nTotal Working Days ::"+totalWorkingDays);
         }
-        totalSalaryPerMonth = wage * EMP_WAGE_PER_HOUR;
-        System.out.println("The Employee total Wage per month :: "+ totalSalaryPerMonth);
-        System.out.println("----------------------------------------");
+        totalEmpWage = totalEmpHrs * empRatePerHour;
     }
 
+    public String toString() {
+            return "Total Emp Wage for Company :: " +company+ " is :: " +totalEmpWage;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the Employees Wage ...");
-
-        EMPWage("Google", 20, 20, 100);
-        EMPWage("FaceBook", 20, 10, 50);
+        EmpWage jio = new EmpWage("jio", 20,2, 10);
+        EmpWage DMart = new EmpWage("DMart", 10, 4, 20);
+        jio.computeEmpWage();
+        System.out.println(jio);
+        DMart.computeEmpWage();
+        System.out.println(DMart);
     }
 }
