9d860000f8674105641f282c97e38d5db3e77fed
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-11-16 20:04:42 +0530

    UC13 - Store Daily Wage

--- src/com/bridgelab/EmpWage.java
+++ src/com/bridgelab/EmpWage.java
@@ -1,5 +1,5 @@
 package com.bridgelab;
-// UC-12 Refactor to have list of multiple companies to manage Employee Wage.
+// UC-13 Store the Daily Wage along with the Total Wage
 import java.util.*;
 import static java.lang.Math.random;
 class EmpWage implements EmpInterface {
